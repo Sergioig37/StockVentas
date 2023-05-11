a69c58dd1c2f4d4ee16de4f566b1304c50a568b7
Mañana <Maana@DESKTOP-KI0CM61.mshome.net>
2023-05-11 08:41:41 +0200

    stock v1 (inacabada)

---
+++ StockVentas/src/datos/InfoFichero.java
@@ -0,0 +1,10 @@
+package datos;
+
+import java.util.HashMap;
+
+import objetos.Producto;
+
+public class InfoFichero {
+
+	private HashMap<String,Producto> productos;
+}
---
+++ StockVentas/src/datos/Read.java
@@ -0,0 +1,14 @@
+package datos;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class Read {
+	
+	
+
+	public void readFile() {
+		
+			
+	}
+}
